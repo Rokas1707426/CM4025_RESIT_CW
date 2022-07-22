c73b3852e55036feff1adfaa495e890c188b7725
Rokas1707426 <1707426@rgu.ac.uk>
2022-07-22 06:43:37 +0300

    db

--- server.js
+++ server.js
@@ -8,28 +8,7 @@ const DBURI = process.env.DBURI || "mongodb+srv://admin123:admin123@users.ibzed.
 const MongoClient = require('mongodb').MongoClient;
 const uri = DBURI;
 
-// This is for using a local db using mongodb community edition.
-// To use the (local) community edition, download the apps from https://www.mongodb.com/try/download/community
-// You will need to either add the files in bin to your path or otherwise access them.
-// Create a directory for your database to live in.
-// From a cmdline, use
-// ./mongod --dbpath ../data/db
-// Where you replace ../data/db with the path to your database (if it's in your path, you might use mongod without the ./).
-// This will start a database that you can access with the following uri (which should be an environment variable):
-//const uri = "mongodb://127.0.0.1:27017";
-// Run the server and add some stuff to the database.
-// use
-// ./mongo 127.0.0.1:27017 // (assuming you're using the default port)
-// from inside the mongo shell, use the following commands to find your database and view the contents
-// show dbs
-// use mydb //(replace mydb with your database name)
-// show collections
-// db.myCollection.find()
-// This will show you the JSON contents of your collection and will verify that your input was written.
-// MongoAtlas has the same sort of set up, but it's a nice http interface
-// Manohar's answer: https://stackoverflow.com/questions/5900792/how-to-view-document-fields-in-mongo-shell
-// https://docs.mongodb.com/manual/reference/mongo-shell/
-//
+
 
 
 app.route('/login')
