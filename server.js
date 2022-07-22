e58c67719710c7e33a393409f74212d6e2a71040
Rokas1707426 <1707426@rgu.ac.uk>
2022-07-22 06:31:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var app     = express();
 const PORT = process.env.PORT || 8080;
-const DBURI = process.env.DBURI || "mongodb+srv://admin123:admin123@users.ibzed.mongodb.net/?retryWrites=true&w=majority";
+const DBURI = process.env.DBURI || "mongodb://127.0.0.1:27017";
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = DBURI;
