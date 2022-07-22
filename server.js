c8fa460d041d2471f2370a6e5dcb68fcf21dfc05
Rokas1707426 <1707426@rgu.ac.uk>
2022-07-22 04:31:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,17 +12,16 @@ app.get('/', function(req, res) {
 //get an instance of the router
 var adminRouter = express.Router();
 ///////////////////////////////////////////////////////////////////
-
-// admin main page. the dashboard (http://localhost:PORT/admin)
-adminRouter.get('/', function(req, res) {
- res.send('I am the dashboard!'); });
- // route middleware that will happen on every request
+// route middleware that will happen on every request
 adminRouter.use(function(req, res, next) {
     // log each request to the console
     console.log(req.method, req.url);
     // continue doing what we were doing and go to the route
     next(); });
    ///////////////////////////////////////////////////////////////////
+// admin main page. the dashboard (http://localhost:PORT/admin)
+adminRouter.get('/', function(req, res) {
+ res.send('I am the dashboard!'); });
 // users page (http://localhost:PORT/admin/users)
 adminRouter.get('/users', function(req, res) {
  res.send('I show all the users!'); });
